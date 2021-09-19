efd15d3b27dd7ab68a695529bb2ce92ae1e4cceb
unknown <nirupama1210@gmail.com>
2021-09-19 21:48:39 +0530

    PROGRAD 5622

--- CALCULATING SP/src/index.js
+++ CALCULATING SP/src/index.js
@@ -3,7 +3,8 @@
 
 // Write a Function to return compute the doundof selling price.
 function find_SellingPrice(a,b){
-    
-    return null;
+    var sp=((100-b)/100)*a;
+    sp=Math.round(sp);
+    return sp;
 }
 //Use SpecRunner to check the Test Cases.
